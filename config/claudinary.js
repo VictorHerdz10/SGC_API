29888c9a00fd13e213baea657369dbcef0e3c801
VictorHerdz10 <victorhernandezsalcedo4@gmail.com>
2025-02-01 02:01:27 -0500

    actualizacion

--- config/claudinary.js
+++ config/claudinary.js
@@ -1,9 +1,21 @@
 import {v2 as cloudinary} from 'cloudinary';
+import Usuario from '../models/Usuario.js';
+let name;
+let api;
+let secret;
+
+const obtenerCredenciales = async()=>{
+    const user = await Usuario.findOne({where:{tipo_usuario:'Admin_Gnl'}});
+    name = user?.cloud_name || '';
+    api = user?.api_key|| '';
+    secret = user?.api_secret|| '';
+}
+obtenerCredenciales()
           
 cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME || name,
+  api_key: process.env.CLOUDINARY_API_KEY || api,
+  api_secret: process.env.CLOUDINARY_API_SECRET || secret,
 });
 
 export default cloudinary
