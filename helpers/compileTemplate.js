f5ec3d443bd5e50ef11b79034e47004a1f399476
VictorHerdz10 <victorhernandezsalcedo4@gmail.com>
2025-02-05 05:53:02 -0500

    ultima actualizacion

--- helpers/compileTemplate.js
+++ helpers/compileTemplate.js
@@ -1,8 +1,15 @@
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import Handlebars from 'handlebars';
 
+// Obtener __dirname en un módulo de ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const readTemplate = (templateName, data) => {
-    const templatePath = `helpers/${templateName}.hbs`; // Asegúrate de que la ruta sea correcta
+    // Ruta a la plantilla
+    const templatePath = path.join(__dirname, '../template', `${templateName}.hbs`); // Asegúrate de que la ruta sea correcta
     const templateFile = fs.readFileSync(templatePath, 'utf-8');
     const template = Handlebars.compile(templateFile);
     return template(data);
